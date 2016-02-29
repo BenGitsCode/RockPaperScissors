f85861db0c7dfa4e7a14b3fe490afe09f20dadc5
Benjamski <valosomnio@gmail.com>
2016-02-29 14:54:20 -0500

    reboot
    Revert to original and start over

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -22,74 +22,33 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    var move;
-    if (move === null) {
-      return (getInput())
-    } else if (move === "rock") {
-      return ("rock")
-    }
-    //pretty sure all these null ifs are unnnecessary after the first one
-    if (move === null) {
-      return (getInput())
-    } else if (move === "paper") {
-      return ("paper")
-    }
-    if (move === null) {
-      return (getINput())
-    } else if( move === "scissors") {
-      return ("scissors")
-    }
-  }
-  // Write an expression that operates on a variable called `move`
-  // If a `move` has a value, your expression should evaluate to that value.
-  // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-  return /* Your Expression */;
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    return /* Your Expression */;
 }
-
 
 function getComputerMove(move) {
-  if (move === null) {
-    return (getInput())
-  } else if (move === "paper") {
-    return "paper"
-  } else if (move === "rock") {
-    return "rock" {
-    } else if (move === "scissors") {
-      return "scissors" {}
-    }
-
-  }
-  // Write an expression that operates on a variable called `move`
-  // If a `move` has a value, your expression should evaluate to that value.
-  // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-  return /* Your Expression */;
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    return /* Your Expression */;
 }
 
-
-
 function getWinner(playerMove,computerMove) {
-  var winner;
-  if (playerMove === computerMove) {
-    return "It's a tie!"
-  } else if (playerMove === "rock" && computerMove === "scissors") {
-    return " You win! Rock beats scissors."
-  } else if (playerMove === "rock" && computerMove === "paper") {
-      return  "Computer wins! Paper beats rock."
-
-  }
-
-  // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-  // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-  // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-  /* YOUR CODE HERE */
-  return winner;
+    var winner;
+    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    /* YOUR CODE HERE */
+    return winner;
 }
 
 function playToFive() {
-  console.log("Let's play Rock, Paper, Scissors");
-  var playerWins = 0;
-  var computerWins = 0;
-  // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-  /* YOUR CODE HERE */
-  return [playerWins, computerWins];
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    /* YOUR CODE HERE */
+    return [playerWins, computerWins];
 }
