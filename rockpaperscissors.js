ecd1288deeaa098cb20f565e3c09f7865e83ac7f
Benjamski <valosomnio@gmail.com>
2016-03-02 17:44:11 -0500

    removed stock comments

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -32,10 +32,6 @@ else
 }
    return move;
 }}
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-
 
 
 
