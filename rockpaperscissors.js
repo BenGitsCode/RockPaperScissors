e79fcca4bda283810a150248511cf5a1c67fc30c
Benjamski <valosomnio@gmail.com>
2016-03-04 10:57:25 -0500

    updated
    so far

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -101,37 +101,36 @@ function getWinner(playerMove,computerMove) {
       console.log("Let's play Rock, Paper, Scissors");
       var playerWins = 0;
       var computerWins = 0;
-      var round = 0;
     while (playerWins < 5 && computerWins < 5) {
         var playerMove = getPlayerMove();
         var computerMove = getComputerMove();
         var winner = getWinner(playerMove, computerMove);
             if (winner === 'player') {
-              console.log("round: " + round + 'Player chose ' + playerMove + ' while computer chose ' + computerMove + '.' + 'Player wins! The score is ' + 'Player ' + playerwins + ' to ' + 'Computer: ' + computerWins);
-                return playerWins =+ 1;
-                return round += 1;
+              console.log('Player chose ' + playerMove + ' while computer chose ' + computerMove + '.' + 'Player wins! The score is ' + 'Player ' + playerwins + ' to ' + 'Computer: ' + computerWins);
+                return playerWins += 1;
+
 
             }
             else if (winner === 'computer')
             {
-              console.log("round: " + round + 'Player chose ' + playerMove + ' while computer chose ' + computerMove + '.' + 'Computer wins! The score is ' + 'Player ' + playerwins + ' to ' + 'Computer: ' + computerWins);
-                return playerWins =+ 1;
-                return round += 1;
+              console.log('Player chose ' + playerMove + ' while computer chose ' + computerMove + '.' + 'Computer wins! The score is ' + 'Player ' + playerwins + ' to ' + 'Computer: ' + computerWins);
+                return playerWins += 1;
+
 
             }
             else
             {
-              console.log("round: " + round + 'It is a Tie! The score is still Player: ' + playerWins + ' to ' +' Computer: ' + computerWins);
-              return round +=1;
+              console.log('It is a Tie! The score is still Player: ' + playerWins + ' to ' +' Computer: ' + computerWins);
+
             }
       if (playerWins === 5)
       {
-        console.log('Round: ' + round + 'Player wins!' + ' The final score is ' + playerWins + ' to ' + computerWins);
+        console.log('Player wins!' + ' The final score is ' + playerWins + ' to ' + computerWins);
 
       }
       else if (computerWins === 5)
       {
-        console.log('Round: ' + round + 'Compter wins!' + ' The final score is ' + playerWins + ' to ' + computerWins);
+        console.log('Compter wins!' + ' The final score is ' + playerWins + ' to ' + computerWins);
       }
 
     }
